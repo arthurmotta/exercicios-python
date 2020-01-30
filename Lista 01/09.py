35a70ad1e748456bd41a3560937dd950a34bcbb6
Arthur Batista Motta de Almeida <arthur.almeida@anac.gov.br>
2020-01-30 14:26:19 -0300

    lista de exercicios 01 - python

---
+++ Lista 01/09.py
@@ -0,0 +1,9 @@
+#9.	Crie um algoritmo que calcule a área de um quadrado,
+#  sendo que o comprimento do lado é informado pelo usuário. 
+# A área do quadrado é calculada elevando-se o lado ao quadrado.
+
+lado = float(input("Digite o comprimento de um lado do quadrado: "))
+
+final = lado**2 
+
+print(f"{final}")
