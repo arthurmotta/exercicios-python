befb7c867591d8eed50d23ee1eb9a3ddce51214b
Arthur Batista Motta de Almeida <arthur.almeida@anac.gov.br>
2020-01-31 10:46:45 -0300

    lista de exercicios 01 - python

---
+++ Lista 01/10.py
@@ -0,0 +1,10 @@
+#10.	Crie um algoritmo que calcule a área de um retângulo, 
+# sendo que os comprimentos da altura e da base são informados pelo usuário. 
+# A área do retângulo é calculada multiplicando-se a altura pela base.
+
+altura = float(input("Digite a altura do retângulo: "))
+base = float(input("Digite a base do retângulo: "))
+
+area = altura * base
+
+print(f"A area do retângulo é de: {area}")
