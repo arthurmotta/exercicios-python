2fefce422f2501db300fff1713985237d793cf77
Arthur Batista Motta de Almeida <arthur.almeida@anac.gov.br>
2020-01-31 14:15:38 -0300

    lista de exercicios 02 - python

---
+++ Lista 02/02.py
@@ -0,0 +1,8 @@
+#2.	 Crie um algoritmo que leia um número e mostre se o mesmo é par ou ímpar.
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
