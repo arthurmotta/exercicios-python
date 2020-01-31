0f19c5d211dee29faeeb4d4485f2c05b4de853d8
Arthur Batista Motta de Almeida <arthur.almeida@anac.gov.br>
2020-01-31 11:02:10 -0300

    lista de exercicios 01 - python

---
+++ Lista 01/11.py
@@ -0,0 +1,9 @@
+#11.	Crie um algoritmo que calcule a área de um círculo, 
+# sendo que o comprimento do raio é informado pelo usuário. 
+# A área do círculo é calculada multiplicando-se pi e o raio ao quadrado.
+
+raio = float(input("Digite o comprimento do raio: "))
+
+area = (raio**2) * 3.14
+
+print(f"A área do círculo é:{area}")
